00cd71fd37518d3386740ddb890e0f6df0656707
Diego Garcia <diegogarciag63@gmail.com>
2022-12-08 17:58:44 -0500

    Add product function modificated, for better js written practices

--- js/addProduct.js
+++ js/addProduct.js
@@ -3,18 +3,49 @@
     let btn = document.querySelector(".cart-btn");
     let productsBox = document.getElementById("populars");
 
+    const createImage = () =>{
+        const imageBox = document.createElement("a");
+        imageBox.classList.add("product__image--box");
+        imageBox.href = "./products/prt-00001.html";
+
+        const img = document.createElement("img");
+        img.src = "../assets/img/products/m-proccesors/5.jpg";
+        img.alt = "product-image"
+
+        imageBox.appendChild(img)
+
+        return imageBox;
+    };
+
+    const productTitle = () =>{
+        const title = document.createElement("h3");
+        title.classList.add("product__card-title");
+        title.innerText = "product title sample";
+        return title;
+    }
+
+    const productPrice = () => {
+        const price = document.createElement("p");
+        price.classList.add("product__card-price");
+        price.innerText = "$ 10.000";
+        return price;
+    };
+
+    const productLink = () => {
+        const link = document.createElement("a")
+        link.classList.add("product__card-link");
+        link.href = "../products/prt-00001.html";
+        link.innerText = "Ver mas...";
+        return link;
+    };
+     
     const createProduct = () => {
         const product = document.createElement("div");
         product.classList.add("product__card", "flex__column");
-
-        const contenido = `
-            <a href="./products/prt-00001.html" class="product__image--box"><img src="./assets/img/products/ryzen-5-5600g.jpg" alt="product"></a>
-            <h3 class="product__card-title">product title sample</h3>
-            <p class="product__card-price">$ 10.000</p>
-            <a href="./products/prt-00001.html" class="product__card-link">Ver mas...</a>
-        `;
-
-        product.innerHTML = contenido;
+        product.appendChild(createImage())
+        product.appendChild(productTitle())
+        product.appendChild(productPrice())
+        product.appendChild(productLink())
         productsBox.appendChild(product);
     };
 
